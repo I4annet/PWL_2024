6f09f34a0c80a9e3bd4246deb1cb53f86a84088f
Ivansyah <ivansyahsantoso@gmail.com>
2025-02-19 12:29:00 +0700

    add: menambahkan file AboutController.php

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function __invoke() {
+        return ' Nama : Ivansyah Eka Oktaviadi Santoso <br> Nim : 2341720126';
+    }
+}
