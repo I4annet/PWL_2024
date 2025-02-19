f7143eabe2b4879c883313175404d257d680f0cc
Ivansyah <ivansyahsantoso@gmail.com>
2025-02-19 08:42:04 +0700

    edit: menambahkan route user/name yang dimana name = John

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,11 @@ use Illuminate\Support\Facades\Route;
 //     return 'Nama saya '.$name;
 // });
 
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'. $postId . ' Komentar ke-'. $commentId;
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-'. $postId . ' Komentar ke-'. $commentId;
+// });
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya '.$name;
 });
-
-
 
