b9abe591ae292a2647b28a2453f5bbcb2cf9c094
Ivansyah <ivansyahsantoso@gmail.com>
2025-02-19 08:34:23 +0700

    edit: menambahkan route pesan dan about untuk menampilkan pesan 'Selamat Datang dan 'NIM & nama'

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,22 @@
 use Illuminate\Support\Facades\Route;
 
 
+// Route::get('/hello', function () {
+//     return 'Hello World';                
+// });
 
-Route::get('/hello', function () {
-    return 'Hello World';                
+
+// Route::get('/world', function () {
+//     return 'World';     
+// });
+
+
+
+Route::get('/pesan', function () {
+    return 'Selamat Datang';
 });
 
-
-
-Route::get('/world', function () {
-    return 'World';     
+Route::get('/about', function () {
+    return 'Nim : 2341720126 <br> Nama : Ivansyah Eka Oktaviadi Santoso';
 });
 
