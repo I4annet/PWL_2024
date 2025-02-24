b342cb3e1819b49f0e4412a7c485f5d241014624
Ivansyah <ivansyahsantoso@gmail.com>
2025-02-24 20:07:18 +0700

    add: menambahkan file .blade.php dan folder Category untuk Soal Praktikum

---
+++ POS/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> POS System </title>
+</head>
+<body>
+    <h1>Selamat Datang di POS </h1>
+    <p>Ini adalah halaman utama.</p>
+</body>
+</html>
